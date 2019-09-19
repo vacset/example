c4215199294db58beeffdc32a2dac5f335f76d14
vacset <me@seta.noip.me>
2019-09-19 21:30:49 +0700

    New example
    Add servlet example for downloading file

---
+++ web-upload-download/src/example/UploadDownloadServlet.java
@@ -0,0 +1,57 @@
+package example;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UploadDownloadServlet
+ */
+@WebServlet(name = "ud", urlPatterns = { "/ud" })
+public class UploadDownloadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UploadDownloadServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String downloadContent = "<?xml version=\"1.0\"?><xml></xml>";
+		String downloadName = "sample.xml";
+		
+		response.setContentType("application/octet-stream");
+        
+        // forces download
+        String headerKey = "Content-Disposition";
+        String headerValue = String.format("attachment; filename=\"%s\"", downloadName);
+        response.setHeader(headerKey, headerValue);
+         
+        // obtains response's output stream
+        byte[] buffer = downloadContent.getBytes();
+		response.setContentLength(buffer.length);
+        OutputStream outStream = response.getOutputStream();         
+        outStream.write(buffer, 0, buffer.length);
+	}
+
+}
